b1def8367ebc723e048bc5dcb594e427dec131d6
widcatd <32911694+widcatd@users.noreply.github.com>
2021-07-23 13:35:08 -0500

    Add files via upload

--- new.dart
+++ new.dart
@@ -11,10 +11,11 @@ class Production {
 
 class Grammar {
   Map<String, List<List<String>>> _productions = new Map();
-  List<List<String>> get_first =[];
-  List<String> first=[];
-  List<List<String>> get_next =[];
-  List<String> next=[];
+  List<List<String>> get_first = [];
+  List<String> first = [];
+  List<List<String>> get_next = [];
+  List<String> next = [];
+  bool n_terminal = true;
   //Map<String, List<String>> first = Map();
   //Map<String, List<String>> next = Map();
 
@@ -71,25 +72,17 @@ class Grammar {
     });
   }
 
-  void get_First_(String x){
-    List temp =_productions[x]! as List;
+  void get_First_(String x) {
+    List temp = _productions[x]! as List;
     print(temp);
-    for(var pos in temp)
-    {
+    for (var pos in temp) {
       print(pos);
-    if (_productions.containsKey(pos[0])==true){
-      get_First_(pos[0]);
-      }
-    else{
-      get_first[get_first.length-1].add(pos[0]);
+      if (_productions.containsKey(pos[0]) == true) {
+        get_First_(pos[0]);
+      } else {
+        get_first[get_first.length - 1].add(pos[0]);
       }
     }
-    //if (_productions.containsKey(temp[0])==true){
-    //  get_First_(temp[0]);
-    //}
-    //else{
-    //  get_first[get_first.length-1].add(temp[0]);
-    //}
   }
 
   void getFirsts() {
@@ -99,10 +92,77 @@ class Grammar {
       first.add(x);
       get_first.add([]);
       get_First_(x);
-      
+    }
+  }
+
+  bool regla1() {
+    if (n_terminal == true) {
+      n_terminal = false;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  void regla1_aplicar(String x) {
+    get_next[get_next.length - 1].add('');
+  }
+
+  bool regla2(List temp, String x) {
+    int p = -1;
+    for (var i = 0; i < temp.length; i++) {
+      if (temp[i] == x) {
+        p = i;
+      }
+    }
+    if (p != -1) {
+      if (p == temp.length - 1) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    return false;
+  }
+
+  void regla2_aplicar(String keyA, String keyB) {
+    int ka = 0;
+    int kb = 0;
+    for (var i = 0; i < next.length; i++) {
+      if (next[i] == keyA) {
+        ka = i;
+      }
+      if (next[i] == keyB) {
+        kb = i;
+      }
+    }
+    get_next[kb] = get_next[ka];
+  }
+
+  void get_next_(String x) {
+    List temp = _productions[x]! as List;
+    print(temp);
+    for (var pos in temp) {
+      print(pos);
+      if (_productions.containsKey(pos[0]) == true) {
+        get_First_(pos[0]);
+      } else {
+        get_first[get_first.length - 1].add(pos[0]);
+      }
+    }
+  }
+
+  void get_nexts() {
+    for (var x in _productions.keys) {
+      //print('Firsts of $x :');
+      //print(_productions[x]);
+      next.add(x);
+      get_next.add([]);
+      get_next_(x);
     }
   }
 }
+
 main() {
   Grammar grammar = Grammar();
   grammar.fromText(grammarText);
